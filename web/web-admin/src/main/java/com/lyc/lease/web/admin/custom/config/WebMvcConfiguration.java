0cb016f261bdee445078a93424cdb0beee2cf150
LuKey_C <3114541245@qq.com>
2025-02-01 15:53:33 +0800

    增加 标签管理功能以及自定义类型转换器

---
+++ web/web-admin/src/main/java/com/lyc/lease/web/admin/custom/config/WebMvcConfiguration.java
@@ -0,0 +1,31 @@
+package com.lyc.lease.web.admin.custom.config;
+
+import com.lyc.lease.web.admin.custom.converter.StringToItemTypeConverter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @ClassName WebMvcConfiguration
+ * @Description TODO SpringMVC配置类
+ * @Author LuKey_C
+ * @Date 2025/2/1 15:50
+ * @Version 1.0
+ */
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
+
+    @Autowired
+    StringToItemTypeConverter stringToItemTypeConverter;
+
+    /**
+     * 添加自定义类型转换器
+     *
+     * @param registry
+     */
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(stringToItemTypeConverter);
+    }
+}
