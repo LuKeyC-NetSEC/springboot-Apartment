352bd94015f3be787c5556c4135dfa572fb7a3f9
LuKey_C <3114541245@qq.com>
2025-02-01 09:46:34 +0800

    隐藏 多余的序列化字段

--- model/src/main/java/com/lyc/lease/model/entity/BaseEntity.java
+++ model/src/main/java/com/lyc/lease/model/entity/BaseEntity.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableLogic;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
@@ -19,15 +20,18 @@ public class BaseEntity implements Serializable {
 
     @Schema(description = "创建时间")
     @TableField(value = "create_time")
+    @JsonIgnore
     private Date createTime;
 
     @Schema(description = "更新时间")
     @TableField(value = "update_time")
+    @JsonIgnore
     private Date updateTime;
 
     @Schema(description = "逻辑删除")
     @TableField("is_deleted")
     @TableLogic //开启逻辑删除
+    @JsonIgnore
     private Byte isDeleted;
 
 }
