ddc6ed0dc0284d059a447e044ab755f1389514d2
LuKey_C <3114541245@qq.com>
2025-02-02 16:28:54 +0800

    增加 MinIO配置及客户端

---
+++ common/src/main/java/com/lyc/lease/common/minio/MinioConfiguration.java
@@ -0,0 +1,34 @@
+package com.lyc.lease.common.minio;
+
+import io.minio.MinioClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @ClassName MinioConfiguration
+ * @Description TODO Minio配置类
+ * @Author LuKey_C
+ * @Date 2025/2/2 16:06
+ * @Version 1.0
+ */
+
+@Configuration
+@EnableConfigurationProperties(MinioProperties.class) //注册属性类
+//@ConfigurationPropertiesScan("com.lyc.lease.common") //注册属性类
+public class MinioConfiguration {
+
+    @Autowired
+    MinioProperties properties;
+
+    @Bean
+    public MinioClient minioClient() {
+        MinioClient minioClient = MinioClient.builder()
+                .endpoint(properties.getEndpoint())
+                .credentials(properties.getAccessKey(), properties.getSecretKey())
+                .build();
+        return minioClient;
+    }
+}
