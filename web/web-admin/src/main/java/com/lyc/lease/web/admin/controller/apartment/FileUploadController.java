fcc4b047e8767bd10a5b092c3b8bb7989627cd8b
LuKey_C <3114541245@qq.com>
2025-02-02 17:05:43 +0800

    增加 文件上传至MinIO功能

--- web/web-admin/src/main/java/com/lyc/lease/web/admin/controller/apartment/FileUploadController.java
+++ web/web-admin/src/main/java/com/lyc/lease/web/admin/controller/apartment/FileUploadController.java
@@ -2,13 +2,16 @@ package com.lyc.lease.web.admin.controller.apartment;
 
 
 import com.lyc.lease.common.result.Result;
+import com.lyc.lease.web.admin.service.FileService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import java.io.InputStream;
 
 
 @Tag(name = "文件管理")
@@ -16,10 +19,21 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 public class FileUploadController {
 
+    @Autowired
+    FileService fileService;
+
+    /**
+     * 上传文件
+     *
+     * @param file 用户上传的文件
+     * @return Result<String>
+     */
     @Operation(summary = "上传文件")
     @PostMapping("upload")
     public Result<String> upload(@RequestParam MultipartFile file) {
-        return Result.ok();
+//        InputStream stream = file.getInputStream();
+        String url = fileService.upload(file);
+        return Result.ok(url);
     }
 
 }
