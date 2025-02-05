af6d8899e015d30a6a2f90f7302a6def32597139
LuKey_C <3114541245@qq.com>
2025-02-05 16:59:02 +0800

    增加 预约看房管理功能

--- web/web-admin/src/main/java/com/lyc/lease/web/admin/service/ViewAppointmentService.java
+++ web/web-admin/src/main/java/com/lyc/lease/web/admin/service/ViewAppointmentService.java
@@ -1,5 +1,6 @@
 package com.lyc.lease.web.admin.service;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.lyc.lease.model.entity.ViewAppointment;
 import com.lyc.lease.web.admin.vo.appointment.AppointmentQueryVo;
 import com.lyc.lease.web.admin.vo.appointment.AppointmentVo;
@@ -13,4 +14,5 @@ import com.baomidou.mybatisplus.extension.service.IService;
 */
 public interface ViewAppointmentService extends IService<ViewAppointment> {
 
+    IPage<AppointmentVo> pageAppointment(Page<AppointmentVo> page, AppointmentQueryVo queryVo);
 }
