756a40518722fe5796e31a1a6b37a06e7c7c6746
LuKey_C <3114541245@qq.com>
2025-02-06 16:49:28 +0800

    增加 租赁协议管理 保存或修改租约信息 根据条件分页查询租约列表 根据id查询租约信息 根据id更新租约状态

--- web/web-admin/src/main/java/com/lyc/lease/web/admin/controller/lease/LeaseAgreementController.java
+++ web/web-admin/src/main/java/com/lyc/lease/web/admin/controller/lease/LeaseAgreementController.java
@@ -1,14 +1,18 @@
 package com.lyc.lease.web.admin.controller.lease;
 
 
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.lyc.lease.common.result.Result;
 import com.lyc.lease.model.entity.LeaseAgreement;
 import com.lyc.lease.model.enums.LeaseStatus;
+import com.lyc.lease.web.admin.service.LeaseAgreementService;
 import com.lyc.lease.web.admin.vo.agreement.AgreementQueryVo;
 import com.lyc.lease.web.admin.vo.agreement.AgreementVo;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -17,22 +21,29 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/admin/agreement")
 public class LeaseAgreementController {
 
+    @Autowired
+    LeaseAgreementService leaseAgreementService;
+
     @Operation(summary = "保存或修改租约信息")
     @PostMapping("saveOrUpdate")
     public Result saveOrUpdate(@RequestBody LeaseAgreement leaseAgreement) {
+        leaseAgreementService.saveOrUpdate(leaseAgreement);
         return Result.ok();
     }
 
     @Operation(summary = "根据条件分页查询租约列表")
     @GetMapping("page")
     public Result<IPage<AgreementVo>> page(@RequestParam long current, @RequestParam long size, AgreementQueryVo queryVo) {
-        return Result.ok();
+        IPage<AgreementVo> page = new Page<>(current, size);
+        IPage<AgreementVo> result = leaseAgreementService.pageAgreementByQuery(page, queryVo);
+        return Result.ok(result);
     }
 
     @Operation(summary = "根据id查询租约信息")
     @GetMapping(name = "getById")
     public Result<AgreementVo> getById(@RequestParam Long id) {
-        return Result.ok();
+        AgreementVo result = leaseAgreementService.getAgreementById(id);
+        return Result.ok(result);
     }
 
     @Operation(summary = "根据id删除租约信息")
@@ -44,6 +55,10 @@ public class LeaseAgreementController {
     @Operation(summary = "根据id更新租约状态")
     @PostMapping("updateStatusById")
     public Result updateStatusById(@RequestParam Long id, @RequestParam LeaseStatus status) {
+        LambdaUpdateWrapper<LeaseAgreement> updateWrapper = new LambdaUpdateWrapper<>();
+        updateWrapper.eq(LeaseAgreement::getId,id);
+        updateWrapper.set(LeaseAgreement::getStatus,status);
+        leaseAgreementService.update(updateWrapper);
         return Result.ok();
     }
 
--- web/web-admin/src/main/java/com/lyc/lease/web/admin/service/impl/LeaseAgreementServiceImpl.java
+++ web/web-admin/src/main/java/com/lyc/lease/web/admin/service/impl/LeaseAgreementServiceImpl.java
@@ -1,9 +1,14 @@
 package com.lyc.lease.web.admin.service.impl;
 
-import com.lyc.lease.model.entity.LeaseAgreement;
-import com.lyc.lease.web.admin.mapper.LeaseAgreementMapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.lyc.lease.model.entity.*;
+import com.lyc.lease.web.admin.mapper.*;
 import com.lyc.lease.web.admin.service.LeaseAgreementService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.lyc.lease.web.admin.vo.agreement.AgreementQueryVo;
+import com.lyc.lease.web.admin.vo.agreement.AgreementVo;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -15,6 +20,43 @@ import org.springframework.stereotype.Service;
 public class LeaseAgreementServiceImpl extends ServiceImpl<LeaseAgreementMapper, LeaseAgreement>
         implements LeaseAgreementService {
 
+    @Autowired
+    LeaseAgreementMapper leaseAgreementMapper;
+
+    @Autowired
+    ApartmentInfoMapper apartmentInfoMapper;
+
+    @Autowired
+    RoomInfoMapper roomInfoMapper;
+
+    @Autowired
+    PaymentTypeMapper paymentTypeMapper;
+
+    @Autowired
+    LeaseTermMapper leaseTermMapper;
+
+    @Override
+    public AgreementVo getAgreementById(Long id) {
+        LeaseAgreement leaseAgreement = leaseAgreementMapper.selectById(id);
+        ApartmentInfo apartmentInfo = apartmentInfoMapper.selectById(leaseAgreement.getApartmentId());
+        RoomInfo roomInfo = roomInfoMapper.selectById(leaseAgreement.getRoomId());
+        PaymentType paymentType = paymentTypeMapper.selectById(leaseAgreement.getPaymentTypeId());
+        LeaseTerm leaseTerm = leaseTermMapper.selectById(leaseAgreement.getLeaseTermId());
+
+        AgreementVo agreementVo = new AgreementVo();
+        BeanUtils.copyProperties(leaseAgreement,agreementVo);
+        agreementVo.setApartmentInfo(apartmentInfo);
+        agreementVo.setRoomInfo(roomInfo);
+        agreementVo.setPaymentType(paymentType);
+        agreementVo.setLeaseTerm(leaseTerm);
+
+        return agreementVo;
+    }
+
+    @Override
+    public IPage<AgreementVo> pageAgreementByQuery(IPage<AgreementVo> page, AgreementQueryVo queryVo) {
+        return leaseAgreementMapper.pageAgreementByQuery(page, queryVo);
+    }
 }
 
 
